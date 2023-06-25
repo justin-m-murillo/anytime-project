a1088153a374be206ca6b7dd2d8dec8faa74d818
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-06-25 00:04:59 -0700

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel", "macros"],
+    plugins: [
+      "nativewind/babel", 
+      "macros",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
